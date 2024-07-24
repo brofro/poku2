024b652ddd4ffb901aa32e3593227fcf8250bf53
Jette Cantiller <jettecantiller@gmail.com>
2024-07-24 13:58:03 -0400

    Add new components and card data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import BattleField from './components/Battlefield.js';
+import { initialCardData, PLAYER_ONE, PLAYER_TWO } from './data/cardData';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BattleField 
+        player1Cards={initialCardData[PLAYER_ONE]}
+        player2Cards={initialCardData[PLAYER_TWO]}
+      />
     </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+// src/constants.js
+export const ATK_ICON = "https://i.imgur.com/X4Fo2c5.png";
+export const HP_ICON = "https://i.imgur.com/MP4IoUo.png";
