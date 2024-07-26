d56a9984c077f7630db330e1f175e7bfd403b07d
Jette Cantiller <jettecantiller@gmail.com>
2024-07-26 17:28:08 -0400

    add hp glow when health changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useRef, React } from 'react';
 import { ATK_ICON, HP_ICON, getImageUrl } from '../constants';
 import './Card.css';
 import rangedicon from './ranged.svg'
@@ -6,6 +6,17 @@ import deathrattleicon from './deathrattle.svg';
 import divineshieldicon from './divineshield.svg';
 
 const Card = ({ id, img, name, atk, hp, currentHp, state, player, index, isAttacking, isCounterAttacking, divineShield, ranged, deathrattleText }) => {
+    const healthRef = useRef(null);
+
+    useEffect(() => {
+        if (healthRef.current) {
+            healthRef.current.classList.add('changed');
+            setTimeout(() => {
+                healthRef.current?.classList.remove('changed');
+            }, 1000);
+        }
+    }, [currentHp]);
+
     // Initialize an array of CSS classes for the card
     const cardClasses = ['card'];
 
@@ -39,7 +50,7 @@ const Card = ({ id, img, name, atk, hp, currentHp, state, player, index, isAttac
                         <span>{atk}</span>
                     </div>
                     {/* Health stat */}
-                    <div className="card-stat">
+                    <div ref={healthRef} className="card-stat health">
                         <img src={HP_ICON} alt="Health" className="stat-icon" />
                         <span>{currentHp}/{hp}</span>
                     </div>
