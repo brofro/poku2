ea2e5a632905ce23db4a821d8c6b1436d0d0ff35
Jette Cantiller <jettecantiller@gmail.com>
2024-07-29 17:12:37 -0400

    fixed redundancy in itemeffect jsx

--- src/components/ItemEffect.js
+++ src/components/ItemEffect.js
@@ -6,9 +6,9 @@ const ItemEffect = ({ icon, alt, text, isShopItem, shopCost }) => (
     <Popover content={text}>
         {isShopItem ?
             <Badge count={shopCost} color={"gold"}>
-                <img src={icon} alt={alt} className={`item-effect-icon ${isShopItem ? 'shop-item' : ''}`} />
+                <img src={icon} alt={alt} className={`item-effect-icon shop-item`} />
             </Badge> :
-            <img src={icon} alt={alt} />}
+            <img src={icon} alt={alt} className={"item-effect-icon"} />}
     </Popover>
 );
 
