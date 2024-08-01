70695d15413e0b3c942b50e7e493f560ce747be3
Jette Cantiller <jettecantiller@gmail.com>
2024-08-01 14:45:54 -0400

    close game loop

--- src/core/App.js
+++ src/core/App.js
@@ -14,7 +14,7 @@ import BattleDebug from "../debugComponents/BattleDebug";
 const Main = (props) => {
   const [page1, setPage1] = useState(true)
   const nextPage = () => setPage1(!page1)
-  const { G, moves } = props
+  const { G, moves, events } = props
 
   function runGame() {
     const { playerResult, gameLog } = runGameLoop({ [PLAYER_ONE]: G.roster, [PLAYER_TWO]: G.P2 }, G.bags, G.gymLevel)
@@ -32,9 +32,17 @@ const Main = (props) => {
     nextPage()
   }
 
+  function isEndGame() {
+    if (G.playerResult === RESULT.LOSE) {
+      events.endGame()
+      alert("YOU LOSE. REFRESH PAGE.")
+    }
+    nextPage()
+  }
 
 
-  return page1 ? <ItemShop {...props} _nextPage={runGame} /> : <BattleDebug {...props} _nextPage={nextPage} />
+
+  return page1 ? <ItemShop {...props} _nextPage={runGame} /> : <BattleDebug {...props} _nextPage={isEndGame} />
 }
 
 const App = Client({
